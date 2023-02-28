ec4676de17d9befb1f842cbe14163913b50544ea
Dani Lipari <lipari.assistenza@gmail.com>
2023-02-28 13:13:42 +0100

    fix connection db

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 8080),
+  port: env.int('PORT', 1337),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'cce4ab3ba3e543517997ede2eb94e6a0'),
